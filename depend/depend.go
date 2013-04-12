70840160278cc09c8db7ea68cce305bc86455c94
Ben Robinson <benjamin.robinson.2013@gmail.com>
2013-04-12 13:38:31 -0400

    Rearranged and added code, not in an executable stage This commit moved the parsing over to the depend package and fleshed out the logic a little bit more.  It did not move the code into an executable stage, however hopefully it will be soon.
    TODO:
     - Complete the parsing function and add tests to ensure that it works
    
     - Complete the make function and add tests for it
    
     - Write up some random c code with some header files and other code
       to have a good base for testing the utility
    
    [Fix #2]

--- depend/depend.go
+++ depend/depend.go
@@ -9,6 +9,11 @@ package depend
 
 import (
   "os"
+  "os/exec"
+  "bufio"
+  "io"
+  "strings"
+  "regexp"
 )
 
 // DependNode is a set of dependencies and associate recipes for a makefile
@@ -21,6 +26,41 @@ type DependNode struct {
 
 // ParseMake opens a makefile, and creates a list of the dependencies within the file
 func ParseMake(filename string) []*DependNode {
+  // Open the makefile
+  file, err := os.Open(filename)
+  if err != nil {
+
+  }
+  defer file.Close()
+
+  // build some regular expressions to identify lines in the file
+	commentLine,err := regexp.Compile("#.*\n")
+	dependencyLine,err:= regexp.Compile("[A-Za-z]*[ ]*:.*\n")
+	recipeLine,err:= regexp.Compile("\t[^#].*\n")
+  // TODO: Add in support for makefile variables
+
+  // the slice of nodes to return
+  dependslice []*DependNode
+
+  // create an object to parse through the makefile
+  scanner := bufio.NewReader(file)
+
+  // begin reading lines from the makefile
+  for line, err := scanner.ReadString('\n'); err != io.EOF {
+    switch {
+      case commentLine.MatchString(line):  // input is a comment so ignore
+      case dependencyLine.MatchString(line):
+        dependnode := new DependNode
+        // for a dependency the proceeding lines need to be checked for recipes
+        dependencyArray := string.Split(line,":")
+        for line, err := scanner.ReadString('\n'); err != io.EOF && recipeLine.MatchString(line) {
+          recipeArray := strings.SplitAfterN(line," ",2)
+          recipe := exec.Command(recipeArray[0], recipeArray[1])
+          dependnode.recipe = append(dependnode.recipe, recipe)
+        }
+
+  }
+
 }
 
 // Make executes the recipes contained in the node after completing those below it
--- make/make.go
+++ make/make.go
@@ -24,42 +24,5 @@ func main() {
 
   listofdependencies := Depend.ParseMake(filename)
 
-  /* Open the makefile */
-  file,err := os.Open(*filename)
-	if err != nil {
-    return
-  }
-  defer file.Close()
-
-  /* create an object to read through the makefile */
-  scanner := bufio.NewReader(file)
-
-	// build regular expressions to match the different cases that appear in the makefile
-	comment,err := regexp.Compile("#.*\n")
-	dependency,err:= regexp.Compile("[A-Za-z]*[ ]*:.*\n")
-	recipe,err:= regexp.Compile("\t[^#].*\n")
-
-
-  /* Begin reading in lines from the makefile */
-  line,err := scanner.ReadString('\n')
-  for err != io.EOF {
-    /* compare to regular expressions */
-    switch {
-      case comment.MatchString(line):
-      // comment so ignore
-      case dependency.MatchString(line):
-      // a dependency, check to see that it has a recipe on the next line
-      // here we need to build a dependency object
-        sourceArray := string.Split(line,":")
-        string.Split(blank[0]," ")
-        string.Split(blank[1]," ")
-        line, err := scanner.ReadString('\n')
-        for err != io.EOF || recipe.MatchString(line) {
-          recipeArray := strings.SplitAfterN(line," ",2)
-          fmt.Printf(recipeArray[0],recipeArray[1])
-          line, err := scanner.ReadString('\n')
-        }
-    }
-    line, err = scanner.ReadString('\n')
 	}
 }
